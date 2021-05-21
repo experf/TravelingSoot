477372bc3cc6f47acee9075aabc6b3807f94ff21
nrser <neil@neilsouza.com>
2021-05-21 16:51:42 -0700

    Spelling correction in a comment

--- Source/TravelingSoot/Public/SootCharacter.h
+++ Source/TravelingSoot/Public/SootCharacter.h
@@ -41,7 +41,7 @@ protected:
   /** Handles moving forward/backward */
   void MoveForward(float Val);
 
-  /** Handles stafing movement, left and right */
+  /** Handles strafing movement, left and right */
   void MoveRight(float Val);
 
   /**
